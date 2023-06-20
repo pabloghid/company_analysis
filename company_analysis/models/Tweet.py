3fca5e4e0f20888507fa5b1a6de116bc28006fc4
pabloghid <pablodg290@gmail.com>
2023-06-20 20:17:03 -0300

    refactored, added search and todos

---
+++ company_analysis/models/Tweet.py
@@ -0,0 +1,5 @@
+class Tweet():
+    def __init__(self, date, url, content):
+        self.date = date
+        self.url = url
+        self.content = content
--- company_analysis/routes.py
+++ company_analysis/routes.py
@@ -1,24 +1,26 @@
-from company_analysis import app
-from flask import render_template
+from .models.Tweet import Tweet
+from . import app
+from flask import render_template, request
 import snscrape.modules.twitter as sntwitter
-
-class Tweet():
-    def __init__(self, date, url, content):
-        self.date = date
-        self.url = url
-        self.content = content
 
 @app.route('/')
 @app.route('/index')
 def index():
-    max_tweets = 10
-    i = 0
-    tweets_list = []
-    scraper = sntwitter.TwitterSearchScraper("Futemax since:2023-06-06 until:2023-06-13")
-    for tweet in scraper.get_items():
-        if i > max_tweets:
-            break
-        tweets_list.append(Tweet(tweet.date, tweet.url, tweet.content))
-        i+=1
+    return render_template('index.html')
 
-    return render_template('list.html', tweets=tweets_list)
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        nome_empresa = request.form.get('nome_empresa')
+        ## TODO: pegar request de max_tweets, since-until, limits e outros filtros
+        max_tweets = 10
+        i = 0
+        tweets_list = []
+        scraper = sntwitter.TwitterSearchScraper(nome_empresa + " since:2023-06-13 until:2023-06-20")
+        for tweet in scraper.get_items():
+            if i > max_tweets:
+                break
+            tweets_list.append(Tweet(tweet.date, tweet.url, tweet.content))
+            i+=1
+
+        return render_template('list.html', tweets=tweets_list)
